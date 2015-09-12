53508240cdc34792844be87dacfcf461098eb38c
Mark Biesheuvel <mail@markbiesheuvel.nl>
2015-09-12 21:17:42 +0200

    Add functions to determine all different hand strengths

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ var Card = require('./lib/Card.js');
 var Hand = require('./lib/Hand.js');
 
 var y = [];
-y.push(new Card('As'));
-y.push(new Card('Ks'));
-y.push(new Card('Qs'));
-y.push(new Card('Js'));
-y.push(new Card('Ts'));
+y.push(new Card('Th'));
+y.push(new Card('4c'));
+y.push(new Card('Ad'));
+y.push(new Card('Td'));
+y.push(new Card('4s'));
 
 var x = new Hand(y);
 
-console.log(x.isStraightFlush());
-console.log(x.isStraight());
-console.log(x.isFlush());
-console.log(x);
+console.log(x.getHandText());
