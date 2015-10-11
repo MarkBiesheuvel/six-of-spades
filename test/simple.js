a2abaf0a49e1d371817152b0ad6d4b1e010168e4
Mark Biesheuvel <mail@markbiesheuvel.nl>
2015-10-11 20:12:31 +0200

    Add tests for exceptions

--- test/simple.js
+++ test/simple.js
@@ -1,4 +1,5 @@
 var Hand = require('../lib/Hand.js');
+var Card = require('../lib/Card.js');
 
 var cases = require('./cases.json');
 var hands = [];
@@ -48,6 +49,47 @@ exports.long_names = function (test) {
     for (var i = 0; i < hands.length; i++) {
         test.equal(hands[i].getLongName(), cases[i].long_name, 'Hand #' + i + ' is a ' + cases[i].long_name);
     }
+
+    test.done();
+};
+
+exports.exceptions = function (test) {
+
+    test.throws(function (){
+        new Hand([]);
+    }, 'No cards');
+
+    test.throws(function (){
+        new Hand(['As', 'Ad', 'Ks', 'Kd']);
+    }, 'Too little cards');
+
+    test.throws(function (){
+        new Hand(['As', 'Ad', 'Ks', 'Kd', 'Qs', 'Qd']);
+    }, 'Too many cards');
+
+    test.throws(function (){
+        new Hand();
+    }, 'No argument');
+
+    test.throws(function (){
+        new Hand('Hello'); // 5 character string (could be confused with array of 5 elements)
+    }, 'Invalid argument');
+
+    test.throws(function (){
+        new Card('1s');
+    }, 'Invalid rank');
+
+    test.throws(function (){
+        new Card('Av');
+    }, 'Invalid suit');
+
+    test.throws(function (){
+        new Card();
+    }, 'No argument');
+
+    test.throws(function (){
+        new Hand(['As', 'Ad', 'Ks', 'Kd', 'Kd']);
+    }, 'Duplicate cards');
 
     test.done();
 };
